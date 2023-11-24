8f668b6638da52cbb95d1f757e4336d37f646841
Eduardo Pinheiro <duedugaranito@gmail.com>
2023-11-23 22:38:53 -0300

    Initial Commit

---
+++ App1/App1App.swift
@@ -0,0 +1,17 @@
+//
+//  App1App.swift
+//  App1
+//
+//  Created by Eduardo Pinheiro on 23/11/23.
+//
+
+import SwiftUI
+
+@main
+struct App1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
